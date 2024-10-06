4aea058c31c75c64f49275a6fd90e8aa6bf92272
khushee12 <156600816+khushee12@users.noreply.github.com>
2024-10-06 12:15:46 +0530

    New changes

--- sthira2/src/components/Community/Chats.jsx
+++ sthira2/src/components/Community/Chats.jsx
@@ -20,14 +20,35 @@ const ProductCard = ({ name, image }) => (
   </div>
 );
 
+const ProductCard1 = ({ name, image }) => (
+
+  <div className="max-w-sm rounded overflow-hidden shadow-lg m-4 p-5  bg-backgroundColor hover:bg-hover cursor-pointer border-2 border-hover">
+    
+    <img className="w-full h-64" src={image} alt={name} />
+    <div className="px-6 py-4">
+      <div className="font-bold text-xl mb-2">{name}</div>
+      
+    </div>
+    
+    <div className="px-6 pt-4 pb-2">
+      <button className="bg-textcolor hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
+        <a href="/Botpage">Continue Chat</a>
+      </button>
+    </div>
+  </div>
+);
+
 const ProductList = () => {
   const products = [
     { name: 'Teens',  image:tt},
     { name: 'Individuals',  image: it },
     { name: 'Couples',  image: ct },
-    { name: 'Couples',  image: ct },
-    
+   products1=[
+      { name: 'Teens',  }
+    ]
   ];
+
+ 
 
   return (
     <div className="bg-backgroundColor2 p-6 rounded-lg shadow-lg text-center">
@@ -36,22 +57,29 @@ const ProductList = () => {
       <p className="text-sm text-backgroundColor mb-4 bold">24 x 7 Assistance | 100% Chat Privacy | Ememrgency Help Available</p>
     
 
-    <div className="flex flex-wrap justify-centre p-3">
+    <div className="flex flex-wrap justify-centre p-3 ml-56">
       
     
       {products.map((product, index) => (
         <ProductCard key={index} {...product} />
-        
       ))}
-
-<div className="bg-backgroundColor p-10 rounded-lg shadow-lg text-center hover:bg-hover  w-full">
-      <h2 className="text-3xl font-bold align-middle text-backgroundColor2 cursor-pointer">My Conversations</h2>
+       <h2 className="text-4xl text-backgroundColor mb-2 pt-20">My Conversations</h2>
 
 
-      </div>
+
+<div className="flex flex-wrap justify-centre p-3 ml-56">
+
+
+{products.map((product1, index) => (
+  <ProductCard1 key={index} {...product1} />
+))}
+        
 
       </div>
     </div>
+    </div>
+
+    
   );
 };
 
